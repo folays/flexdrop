c4a972523c069057ad9ba31730a508b43d245324
root <root@backup-vit-4.online.net>
2013-04-05 01:49:56 +0200

    .

--- insert.pl
+++ insert.pl
@@ -7,7 +7,7 @@ use Getopt::Long qw(:config no_auto_abbrev require_order);
 
 my %long_opts = (verbose => 1, min_mb => 10);
 GetOptions("verbose=i" => \$long_opts{verbose},
-	   "min_mb=i" => \$long_opts{min_mb},
+	   "min-mb=i" => \$long_opts{min_mb},
     ) or die "bad options";
 
 sub run()
